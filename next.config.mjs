35bb5f41a860d821e6b80ac97ffb603a77e20320
Kristoff Borrel <kristoffborrel@gmail.com>
2024-04-10 11:52:17 -0400

    add: media files, projects and profile pic

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+    domains: ['localhost', 'pocketwatch-865fc.appspot.com', 'firebasestorage.googleapis.com'],
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+  experimental: {
+    serverActions: true,
+  },
+};
 
 export default nextConfig;
