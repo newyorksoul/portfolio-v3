b4e3b6faf265d0c5b7ffa49f8acd8cf37ce5a1ba
Kristoff Borrel <kristoffborrel@gmail.com>
2024-04-13 03:39:53 -0400

    feat: profile picture dark mode support

--- app/page.tsx
+++ app/page.tsx
@@ -6,28 +6,37 @@ import Projects from "@/components/projects";
 import Skills from "@/components/skills";
 import Experience from "@/components/experience";
 import Contact from "@/components/contact";
-import { MeshGradientRenderer } from "@johnn-e/react-mesh-gradient";
+import { MeshGradientRenderer } from "@/components/react-mesh-gradient";
 import React from "react";
-//import { colors } from "nice-color-palettes";
+import { useTheme } from "@/context/theme";
+//import { colors as pallete } from "nice-color-palettes";
 
 export default function Home() {
   const [speed, setSpeed] = React.useState(0.01);
   const [isWireframe, setIsWireframe] = React.useState(false);
   const [isDarkBackground, setIsDarkBackground] = React.useState(false);
-  const [color, setColor] = React.useState(Math.floor(Math.random() * 100));
+  const appearance = useTheme();
+
+  const pallette = () => {
+    let lightMode = ["#DE5319", "#1704b", "#dOc7c4", "#f4ba41", "#ae7324"]
+    let darkMode = ["#C3E4FF", "#6EC3F4", "#FE9900", "#B9BEFF", "#B3B8F9"]
+    return (
+      appearance.theme === "light" ? lightMode : darkMode
+    )
+  };
+
   return (
     <div>
       <MeshGradientRenderer
         className="gradient bg-grainy:after absolute top-[-6rem] -z-10 right-[11rem] h-1/4 w-full rounded-full blur-[10rem] sm:w-[68.75rem] "
         //colors={["#DE5319", "#1704b", "#dOc7c4", "#f4ba41", "#ae7324"]}
-        //colors={colors[color]}
-        colors={["#C3E4FF", "#6EC3F4", "#FE9900", "#B9BEFF", "#B3B8F9"]}
+        colors={pallette()}
         speed={speed}
         wireframe={isWireframe}
         backgroundColor={isDarkBackground ? "#000000" : "#FFFFFF"}
 
       />
-      <main className="flex flex-col items-center px-4">
+      <main className="flex flex-col items-center pxa-4">
         <Intro />
         <Section />
         <About />
